12ed0754b19bd8e05df7e76334859cfaf434afd5
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2024-01-08 14:27:44 +0530

    route update

--- backend-js/Main/src/routes/user.routes.js
+++ backend-js/Main/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginuser, logoutUser, register,refereshAccesstoken } from "../controllers/user.controllers.js";
+import { loginuser, logoutUser, register,refereshAccesstoken, changeCurrentpassword, getCurrentUser, updateAccountDet, updateUseravatar, updatusercoverimg, getUserChannelProfile, getwatchHistory } from "../controllers/user.controllers.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
@@ -20,6 +20,14 @@ router.route("/login").post(
 //secure routes
 router.route("/logout").post(verifyJWT,logoutUser)
 router.route("/refreshtoken").post(refereshAccesstoken)
+router.route("/change-pass").post(verifyJWT,changeCurrentpassword)
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+router.route("/update-acc").patch(verifyJWT,updateAccountDet)
+router.route("/avater").patch(verifyJWT,upload.single("avatar"),updateUseravatar)
+router.route("/cover").patch(verifyJWT,upload.single("cover"),updatusercoverimg)
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+router.route("/history").get(verifyJWT,getwatchHistory)
+
 
 
 
